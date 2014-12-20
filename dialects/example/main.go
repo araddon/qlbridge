3d085c74d93db857a8bd9b62d4df66a97e77c398
Aaron Raddon <aaron@getlytics.com>
2014-12-19 21:28:13 -0800

    update example

--- dialects/example/main.go
+++ dialects/example/main.go
@@ -6,11 +6,18 @@ import (
 	"strings"
 )
 
+/*
+	This example is meant to show how to create a new
+	Dialect/Query Language with a keyword"SUBSCRIBETO"
+	then Lex/Consume query
+
+*/
 var (
-	// Tokens Specific to our PUBSUB dialect
+	// We need a token to recognize our "SUBSCRIBETO" keyword
+	// in our PUBSUB dialect
 	TokenSubscribeTo ql.TokenType = 1000
 
-	// We are going to create our own Dialect Right now
+	// We are going to create our own Dialect now
 	//  that uses a "SUBSCRIBETO" keyword
 	pubsub = &ql.Statement{TokenSubscribeTo, []*ql.Clause{
 		{Token: TokenSubscribeTo, Lexer: ql.LexColumns},
@@ -23,10 +30,10 @@ var (
 )
 
 func init() {
-	// We are going to inject new tokens into QLparse describing custom token we created
+	// inject any new tokens into QLBridge.Lex describing the custom tokens we created
 	ql.TokenNameMap[TokenSubscribeTo] = &ql.TokenInfo{Description: "subscribeto"}
 
-	// OverRide the Identity Characters in QLparse to allow a dash in identity
+	// OverRide the Identity Characters in lexer to allow a dash in identity
 	ql.IDENTITY_CHARS = "_./-"
 	ql.LoadTokenInfo()
 
@@ -66,6 +73,8 @@ func LexMaybe(l *ql.Lexer) ql.StateFn {
 	return ql.LexExpressionOrIdentity
 }
 
+func Tok(tok ql.TokenType, val string) ql.Token { return ql.Token{tok, val, 0} }
+
 func main() {
 
 	/* Many *ql languages support some type of columnar layout such as:
@@ -80,29 +89,29 @@ func main() {
 
 	verifyLexerTokens(l,
 		[]ql.Token{
-			{TokenSubscribeTo, "SUBSCRIBETO"},
-			{ql.TokenUdfExpr, "count"},
-			{ql.TokenLeftParenthesis, "("},
-			{ql.TokenIdentity, "x"},
-			{ql.TokenRightParenthesis, ")"},
-			{ql.TokenComma, ","},
-			{ql.TokenIdentity, "Name"},
-			{ql.TokenFrom, "FROM"},
-			{ql.TokenIdentity, "ourstream"},
-			{ql.TokenWhere, "WHERE"},
-			{ql.TokenIdentity, "k"},
-			{ql.TokenEqual, "="},
-			{ql.TokenUdfExpr, "REPLACE"},
-			{ql.TokenLeftParenthesis, "("},
-			{ql.TokenUdfExpr, "LOWER"},
-			{ql.TokenLeftParenthesis, "("},
-			{ql.TokenIdentity, "Name"},
-			{ql.TokenRightParenthesis, ")"},
-			{ql.TokenComma, ","},
-			{ql.TokenValue, "cde"},
-			{ql.TokenComma, ","},
-			{ql.TokenValue, "xxx"},
-			{ql.TokenRightParenthesis, ")"},
-			{ql.TokenEOS, ";"},
+			Tok(TokenSubscribeTo, "SUBSCRIBETO"),
+			Tok(ql.TokenUdfExpr, "count"),
+			Tok(ql.TokenLeftParenthesis, "("),
+			Tok(ql.TokenIdentity, "x"),
+			Tok(ql.TokenRightParenthesis, ")"),
+			Tok(ql.TokenComma, ","),
+			Tok(ql.TokenIdentity, "Name"),
+			Tok(ql.TokenFrom, "FROM"),
+			Tok(ql.TokenIdentity, "ourstream"),
+			Tok(ql.TokenWhere, "WHERE"),
+			Tok(ql.TokenIdentity, "k"),
+			Tok(ql.TokenEqual, "="),
+			Tok(ql.TokenUdfExpr, "REPLACE"),
+			Tok(ql.TokenLeftParenthesis, "("),
+			Tok(ql.TokenUdfExpr, "LOWER"),
+			Tok(ql.TokenLeftParenthesis, "("),
+			Tok(ql.TokenIdentity, "Name"),
+			Tok(ql.TokenRightParenthesis, ")"),
+			Tok(ql.TokenComma, ","),
+			Tok(ql.TokenValue, "cde"),
+			Tok(ql.TokenComma, ","),
+			Tok(ql.TokenValue, "xxx"),
+			Tok(ql.TokenRightParenthesis, ")"),
+			Tok(ql.TokenEOS, ";"),
 		})
 }
