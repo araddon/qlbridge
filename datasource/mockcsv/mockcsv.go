b5c36e0be25bee11921c3c77a3ef642772dbc088
Aaron Raddon <aaron@lytics.io>
2015-08-10 14:58:34 -0700

    comments, doc for mockcsv

--- datasource/mockcsv/mockcsv.go
+++ datasource/mockcsv/mockcsv.go
@@ -13,9 +13,9 @@ import (
 var (
 	_ = u.EMPTY
 
-	// Different Features of this Static Data Source
+	// Different Features of this MockCsv Data Source
 	// - the rest are implemented in the static data source
-	//   which is returned per "table"
+	//   which has a Static per table
 	_ datasource.DataSource = (*MockCsvSource)(nil)
 
 	MockCsvGlobal = NewMockSource()
@@ -29,52 +29,75 @@ func LoadTable(name, csvRaw string) {
 }
 
 type MockCsvSource struct {
-	tables map[string]*membtree.StaticDataSource
-	raw    map[string]string
+	tablenamelist []string
+	tables        map[string]*membtree.StaticDataSource
+	raw           map[string]string
 }
 
 func NewMockSource() *MockCsvSource {
 	return &MockCsvSource{
-		raw:    make(map[string]string),
-		tables: make(map[string]*membtree.StaticDataSource),
+		tablenamelist: make([]string, 0),
+		raw:           make(map[string]string),
+		tables:        make(map[string]*membtree.StaticDataSource),
 	}
 }
+
 func (m *MockCsvSource) Open(tableName string) (datasource.SourceConn, error) {
-	//u.Debugf("MockCsv Open: %q", tableName)
+
 	if tbl, ok := m.tables[tableName]; ok {
-		u.Debugf("found tbl: %v  %v", tableName, tbl.Length())
+		u.Debugf("found mockcsv table:%q  len=%v", tableName, tbl.Length())
 		return tbl, nil
-	} else if csvRaw, ok := m.raw[tableName]; ok {
-		sr := strings.NewReader(csvRaw)
-		u.Debugf("open mockcsv: %v  data:%v", tableName, csvRaw)
-		csvSource, _ := datasource.NewCsvSource(tableName, 0, sr, make(<-chan bool, 1))
-		tbl := membtree.NewStaticData(tableName)
-		tbl.SetColumns(csvSource.Columns())
-		u.Infof("set index col for %v: %v -- %v", tableName, 0, csvSource.Columns()[0])
-		m.tables[tableName] = tbl
-		for {
-			msg := csvSource.Next()
-			if msg == nil {
-				u.Infof("table:%v  len=%v", tableName, tbl.Length())
-				return tbl, nil
-			}
-			dm, ok := msg.Body().(*datasource.SqlDriverMessageMap)
-			if !ok {
-				return nil, fmt.Errorf("Expected []driver.Value but got %T", msg.Body())
-			}
-			tbl.Put(nil, nil, dm.Values())
-		}
+	}
+	err := m.loadTable(tableName)
+	if err == nil {
+		return m.tables[tableName], nil
+	} else {
+		u.Errorf("could not load table %v", err)
+		return nil, err
 	}
 	return nil, datasource.ErrNotFound
 }
-func (m *MockCsvSource) Close() error { return nil }
-func (m *MockCsvSource) Tables() []string {
-	tbls := make([]string, 0, len(m.tables))
-	for tblName, _ := range m.tables {
-		tbls = append(tbls, tblName)
+
+func (m *MockCsvSource) loadTable(tableName string) error {
+
+	csvRaw, ok := m.raw[tableName]
+	if !ok {
+		return datasource.ErrNotFound
 	}
-	return tbls
+	sr := strings.NewReader(csvRaw)
+	u.Debugf("load mockcsv: %q  data:%v", tableName, csvRaw)
+	csvSource, _ := datasource.NewCsvSource(tableName, 0, sr, make(<-chan bool, 1))
+	tbl := membtree.NewStaticData(tableName)
+	tbl.SetColumns(csvSource.Columns())
+	//u.Infof("set index col for %v: %v -- %v", tableName, 0, csvSource.Columns()[0])
+	m.tables[tableName] = tbl
+
+	// Now we are going to page through the Csv Source and Put into
+	//  Static Data Source, ie copy into memory
+	for {
+		msg := csvSource.Next()
+		if msg == nil {
+			u.Infof("table:%v  len=%v", tableName, tbl.Length())
+			return nil
+		}
+		dm, ok := msg.Body().(*datasource.SqlDriverMessageMap)
+		if !ok {
+			return fmt.Errorf("Expected []driver.Value but got %T", msg.Body())
+		}
+
+		// We don't know the Key
+		tbl.Put(nil, nil, dm.Values())
+	}
+	return nil
 }
+
+func (m *MockCsvSource) Close() error     { return nil }
+func (m *MockCsvSource) Tables() []string { return m.tablenamelist }
 func (m *MockCsvSource) SetTable(name, csvRaw string) {
+	if _, exists := m.raw[name]; !exists {
+		m.tablenamelist = append(m.tablenamelist, tableName)
+	}
+	// Even if it exists, replace it?  Which would not work
+	//  because the raw wouldn't get converted to
 	m.raw[name] = csvRaw
 }
