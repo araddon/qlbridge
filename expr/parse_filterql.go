8ef04d47808b9b945102a68524a6376b4ce810bc
Michael Schurter <schmichael@lytics.io>
2015-11-30 13:54:51 -0800

    Improve filterql keyword test and remove offset
    offset wasn't supported by the parser, and I'm not sure it's something
    we want anyway.

--- expr/parse_filterql.go
+++ expr/parse_filterql.go
@@ -31,7 +31,6 @@ type (
 		Filter      *Filters      // A top level filter
 		From        string        // From is optional
 		Limit       int           // Limit
-		Offset      int           // Offset
 		Alias       string        // Non-Standard sql, alias/name of sql another way of expression Prepared Statement
 		With        u.JsonHelper  // Non-Standard SQL for properties/config info, similar to Cassandra with, purse json
 	}
@@ -97,9 +96,6 @@ func (m *FilterStatement) writeBuf(buf *bytes.Buffer) {
 	}
 	if m.Limit > 0 {
 		buf.WriteString(fmt.Sprintf(" LIMIT %d", m.Limit))
-	}
-	if m.Offset > 0 {
-		buf.WriteString(fmt.Sprintf(" OFFSET %d", m.Offset))
 	}
 	if m.Alias != "" {
 		buf.WriteString(fmt.Sprintf(" ALIAS %s", m.Alias))
