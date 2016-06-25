1440af993faec8ce468577dcea75abda363b60b3
Aaron Raddon <aaron@lytics.io>
2016-06-25 14:15:43 -0700

    Sql Select discard spurious comments

--- rel/parse_sql.go
+++ rel/parse_sql.go
@@ -130,6 +130,25 @@ func (m *Sqlbridge) initialComment() string {
 	return comment
 }
 
+func discardComments(m expr.TokenPager) {
+
+	for {
+		// We are going to loop until we find the first Non-Comment Token
+		switch m.Cur().T {
+		case lex.TokenComment, lex.TokenCommentML,
+			lex.TokenCommentStart, lex.TokenCommentHash, lex.TokenCommentEnd,
+			lex.TokenCommentSingleLine, lex.TokenCommentSlashes:
+			// discard
+			m.Next()
+		default:
+			// first non-comment token
+			return
+		}
+
+	}
+	panic("unreachable")
+}
+
 // First keyword was SELECT, so use the SELECT parser rule-set
 func (m *Sqlbridge) parseSqlSelect() (*SqlSelect, error) {
 
@@ -144,15 +163,10 @@ func (m *Sqlbridge) parseSqlSelect() (*SqlSelect, error) {
 	}
 
 	// columns
-	//if m.Cur().T != lex.TokenStar {
 	if err := parseColumns(m, m.funcs, m.buildVm, req); err != nil {
 		u.Debug(err)
 		return nil, err
 	}
-	// } else if err := m.parseSelectStar(req); err != nil {
-	// 	u.Debug(err)
-	// 	return nil, err
-	// }
 
 	//u.Debugf("cur? %v", m.Cur())
 	// select @@myvar limit 1
@@ -173,44 +187,49 @@ func (m *Sqlbridge) parseSqlSelect() (*SqlSelect, error) {
 	}
 
 	// INTO
+	discardComments(m)
 	if errreq := m.parseInto(req); errreq != nil {
 		return nil, errreq
 	}
 
 	// FROM
+	discardComments(m)
 	if errreq := m.parseSources(req); errreq != nil {
 		return nil, errreq
 	}
 
 	// WHERE
+	discardComments(m)
 	if errreq := m.parseWhereSelect(req); errreq != nil {
 		return nil, errreq
 	}
 
 	// GROUP BY
-	//u.Debugf("GroupBy?  : %v", m.Cur())
+	discardComments(m)
 	if errreq := m.parseGroupBy(req); errreq != nil {
 		return nil, errreq
 	}
 
 	// HAVING
-	//u.Debugf("Having?  : %v", m.Cur())
+	discardComments(m)
 	if errreq := m.parseHaving(req); errreq != nil {
 		return nil, errreq
 	}
 
 	// ORDER BY
-	//u.Debugf("OrderBy?  : %v", m.Cur())
+	discardComments(m)
 	if errreq := m.parseOrderBy(req); errreq != nil {
 		return nil, errreq
 	}
 
 	// LIMIT
+	discardComments(m)
 	if err := m.parseLimit(req); err != nil {
 		return nil, err
 	}
 
 	// WITH
+	discardComments(m)
 	with, err := ParseWith(m.SqlTokenPager)
 	if err != nil {
 		return nil, err
@@ -218,6 +237,7 @@ func (m *Sqlbridge) parseSqlSelect() (*SqlSelect, error) {
 	req.With = with
 
 	// ALIAS
+	discardComments(m)
 	if err := m.parseAlias(req); err != nil {
 		return nil, err
 	}
@@ -672,6 +692,8 @@ func (m *Sqlbridge) parseCommand() (*SqlCommand, error) {
 func parseColumns(m expr.TokenPager, fr expr.FuncResolver, buildVm bool, stmt ColumnsStatement) error {
 
 	var col *Column
+
+	discardComments(m)
 
 	for {
 
