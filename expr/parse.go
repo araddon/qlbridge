bb34b08fdc750cc6c4e9dd1d0f496d289404f5d0
Aaron Raddon <aaron@lytics.io>
2016-12-16 17:16:49 -0800

    Expression Negation

--- expr/parse.go
+++ expr/parse.go
@@ -545,12 +545,16 @@ func (t *tree) F(depth int) Node {
 			arg = t.O(depth + 1)
 		case lex.TokenLogicAnd, lex.TokenLogicOr:
 			arg = t.O(depth + 1)
+		case lex.TokenUdfExpr:
+			arg = t.F(depth + 1)
 		default:
 			// NOT <expr> LIKE <expr>
 			// NOT <expr> INTERSECTS <expr>
 			// NOT <expr> BETWEEN <expr> AND <expr>
 			// NOT <expr> CONTAINS <expr>
 			// NOT <expr> IN <expr>
+			//
+			// NOT identity > 7
 			switch t.Peek().T {
 			case lex.TokenIN, lex.TokenLike, lex.TokenContains, lex.TokenBetween:
 				// At one point i switched this to O  ?? which seems wrong to me.
@@ -559,11 +563,12 @@ func (t *tree) F(depth int) Node {
 			case lex.TokenIntersects:
 				arg = t.O(depth + 1)
 			default:
-				arg = t.F(depth + 1)
+				// NOT identity >= 7
+				arg = t.O(depth + 1)
 			}
 		}
 		n := NewUnary(cur, arg)
-		debugf(depth, "f urnary: %s", n)
+		debugf(depth, "f urnary: %s   arg: %#v", n, arg)
 		return n
 	case lex.TokenExists:
 		// Urnary operations:  require right side value node
