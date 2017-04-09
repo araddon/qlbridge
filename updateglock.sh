d1c7897f5d82da512b8d5f2484b00d88c4c899c9
Aaron Raddon <araddon@gmail.com>
2017-04-09 09:37:03 -0700

    Update script for vendoring

--- updateglock.sh
+++ updateglock.sh
@@ -27,15 +27,16 @@ cd $GOPATH/src/github.com/mssola/user_agent && git checkout master && git pull
 cd $GOPATH/src/github.com/pborman/uuid && git checkout master && git pull
 cd $GOPATH/src/github.com/rcrowley/go-metrics && git checkout master && git pull
 
+cd $GOPATH/src/golang.org/x/crypto && git checkout master && git pull
 cd $GOPATH/src/golang.org/x/net && git checkout master && git pull
 cd $GOPATH/src/golang.org/x/text && git checkout master && git pull
+cd $GOPATH/src/golang.org/x/sys && git checkout master && git pull
 cd $GOPATH/src/golang.org/x/oauth2 && git checkout master && git pull
 cd $GOPATH/src/google.golang.org/api && git checkout master && git pull
 cd $GOPATH/src/google.golang.org/genproto && git checkout master && git pull
 cd $GOPATH/src/google.golang.org/grpc && git checkout master && git pull
 cd $GOPATH/src/cloud.google.com/go/ && git checkout master && git pull
 cd $GOPATH/src/github.com/googleapis/gax-go && git checkout master && git pull
-cd $GOPATH/src/golang.org/x/crypto && git checkout master && git pull
 
 #go get -u -v ./...
 
