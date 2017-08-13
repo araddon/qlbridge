cfa5dac6934cdfe2414b7ce8a497fb9102c03ed5
Aaron Raddon <araddon@gmail.com>
2017-08-13 11:46:11 -0700

    code coverage improvements

--- exec/exec_test.go
+++ exec/exec_test.go
@@ -22,87 +22,9 @@ func init() {
 	// load our mock data sources "users", "articles"
 	td.LoadTestDataOnce()
 }
+
 func TestStatements(t *testing.T) {
-
-	// Literal Queries
-	testutil.TestSelect(t, `select 1;`,
-		[][]driver.Value{{int64(1)}},
-	)
-	testutil.TestSelect(t, `select 1, "hello";`,
-		[][]driver.Value{{int64(1), "hello"}},
-	)
-	testutil.TestSelect(t, `select exists(email), email FROM users WHERE yy(reg_date) > 10;`,
-		[][]driver.Value{{true, "aaron@email.com"}},
-	)
-	// - yy func evaluates
-	// - projection (user_id, email)
-	testutil.TestSelect(t, `select user_id, email FROM users WHERE yy(reg_date) > 10;`,
-		[][]driver.Value{{"9Ip1aKbeZe2njCDM", "aaron@email.com"}},
-	)
-	// - ensure we can evaluate against "NULL"
-	// - extra paren in where
-	// - `db`.`col` syntax
-	testutil.TestSelect(t, "SELECT user_id FROM users WHERE (`users.user_id` != NULL)",
-		[][]driver.Value{{"hT2impsabc345c"}, {"9Ip1aKbeZe2njCDM"}, {"hT2impsOPUREcVPc"}},
-	)
-	testutil.TestSelect(t, "SELECT email FROM users WHERE interests != NULL)",
-		[][]driver.Value{{"aaron@email.com"}, {"bob@email.com"}},
-	)
-	testutil.TestSelect(t, "SELECT email FROM users WHERE (`users`.`email` like \"%aaron%\");",
-		[][]driver.Value{{"aaron@email.com"}},
-	)
-
-	// Mixed *, literal, fields
-	testutil.TestSelect(t, "SELECT *, emaildomain(email), contains(email,\"aaron\"), 5 FROM users WHERE email = \"aaron@email.com\"",
-		[][]driver.Value{{"9Ip1aKbeZe2njCDM", "aaron@email.com", "fishing", "2012-10-17T17:29:39.738Z", "82",
-			"email.com", true, int64(5)}},
-	)
-
-	// - user_id != NULL (on string column)
-	// - as well as count(*)
-	testutil.TestSelect(t, "SELECT COUNT(*) AS count FROM users WHERE (`users.user_id` != NULL)",
-		[][]driver.Value{{int64(3)}},
-	)
-
-	// Aliasing columns in group by
-	testutil.TestSelect(t, "select `users`.`user_id` AS userids FROM users WHERE email = \"aaron@email.com\" GROUP BY `users`.`user_id`;",
-		[][]driver.Value{{"9Ip1aKbeZe2njCDM"}},
-	)
-
-	// nested functions in aggregations
-	// - note also lack of group-by ie determine Is Agg query in rel ast
-	testutil.TestSelect(t, "SELECT AVG(CHAR_LENGTH(CAST(`user`.`email` AS CHAR))) AS `len` FROM `users`",
-		[][]driver.Value{{float64(14.0)}}, // aaron@email.combob@email.comnot_an_email_2 = 42 characters / 3 = 14
-	)
-
-	// Distinct keyword
-	testutil.TestSelect(t, "SELECT COUNT(DISTINCT(`users.email`)) AS cd FROM users",
-		[][]driver.Value{{int64(0)}},
-	)
-
-	testutil.TestSelect(t, "SELECT email FROM users ORDER BY email DESC",
-		[][]driver.Value{{"not_an_email_2"}, {"bob@email.com"}, {"aaron@email.com"}},
-	)
-	testutil.TestSelect(t, "SELECT email FROM users ORDER BY email ASC",
-		[][]driver.Value{{"aaron@email.com"}, {"bob@email.com"}, {"not_an_email_2"}},
-	)
-
-	// This is an error because we have schema on this table, and this column
-	// doesn't exist.
-	testutil.TestSelectErr(t, "SELECT email, non_existent_field FROM users ORDER BY email ASC", nil)
-
-	/*
-		// TODO: #56 DISTINCT inside count()
-		testutil.TestSelect(t, "SELECT COUNT(DISTINCT(`users.user_id`)) AS cd FROM users",
-			[][]driver.Value{{int64(3)}},
-		)
-
-		// TODO: #56 this doesn't work because ordering is non-deterministic coming out of group by currently
-		//  which technically don't think there is any sql expectation of ordering, but there is for this test harness
-		testutil.TestSelect(t, "select `users`.`user_id` AS userids FROM users GROUP BY `users`.`user_id`;",
-			[][]driver.Value{{"hT2impsabc345c"}, {"9Ip1aKbeZe2njCDM"}, {"hT2impsOPUREcVPc"}},
-		)
-	*/
+	testutil.RunTestSuite(t)
 }
 
 func TestExecSqlCommands(t *testing.T) {
