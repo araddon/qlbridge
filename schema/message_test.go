7af2d6969c7a442e237152fb634e1c8b4b55ed81
Aaron Raddon <araddon@gmail.com>
2017-10-08 09:28:28 -0700

    Code Coverage on schema

---
+++ schema/message_test.go
@@ -0,0 +1,14 @@
+package schema_test
+
+import (
+	"database/sql/driver"
+	"testing"
+
+	"github.com/araddon/qlbridge/schema"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMessage(t *testing.T) {
+	k := schema.NewKeyUint(uint64(7))
+	assert.Equal(t, driver.Value(uint64(7)), k.Key())
+}
