93a0fcbf3197008c3f374cad2a3f7011588ba7ad
Aaron Raddon <araddon@gmail.com>
2017-10-14 11:42:43 -0700

    Package dependency update

--- updateglock.sh
+++ updateglock.sh
@@ -3,7 +3,6 @@
 
 cd $GOPATH/src/github.com/araddon/dateparse && git checkout master && git pull
 cd $GOPATH/src/github.com/araddon/gou && git checkout master && git pull
-cd $GOPATH/src/github.com/bmizerany/assert && git checkout master && git pull
 cd $GOPATH/src/github.com/couchbaselabs/goforestdb && git checkout master && git pull
 # cd $GOPATH/src/github.com/dataux/dataux && git checkout master && git pull
 cd $GOPATH/src/github.com/dchest/siphash && git checkout master && git pull
@@ -14,6 +13,7 @@ cd $GOPATH/src/github.com/google/btree && git checkout master && git pull
 cd $GOPATH/src/github.com/hashicorp/go-immutable-radix && git checkout master && git pull
 cd $GOPATH/src/github.com/hashicorp/go-memdb && git checkout master && git pull
 cd $GOPATH/src/github.com/hashicorp/golang-lru && git checkout master && git pull
+cd $GOPATH/src/github.com/jmespath/go-jmespath && git checkout master && git pull
 cd $GOPATH/src/github.com/jmoiron/sqlx && git checkout master && git pull
 cd $GOPATH/src/github.com/kr/pretty && git checkout master && git pull
 cd $GOPATH/src/github.com/kr/pty && git checkout master && git pull
@@ -26,6 +26,7 @@ cd $GOPATH/src/github.com/mb0/glob && git checkout master && git pull
 cd $GOPATH/src/github.com/mssola/user_agent && git checkout master && git pull
 cd $GOPATH/src/github.com/pborman/uuid && git checkout master && git pull
 cd $GOPATH/src/github.com/rcrowley/go-metrics && git checkout master && git pull
+cd $GOPATH/src/github.com/stretchr/testify && git checkout master && git pull
 
 cd $GOPATH/src/golang.org/x/crypto && git checkout master && git pull
 cd $GOPATH/src/golang.org/x/net && git checkout master && git pull
@@ -34,7 +35,7 @@ cd $GOPATH/src/golang.org/x/sys && git checkout master && git pull
 cd $GOPATH/src/golang.org/x/oauth2 && git checkout master && git pull
 
 
-# cd $GOPATH/src/google.golang.org/api && git checkout master && git pull
+cd $GOPATH/src/google.golang.org/api && git checkout master && git pull
 # cd $GOPATH/src/google.golang.org/genproto && git checkout master && git pull
 # cd $GOPATH/src/google.golang.org/grpc && git checkout master && git pull
 # cd $GOPATH/src/cloud.google.com/go/ && git checkout master && git pull
