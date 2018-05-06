01e53d8593e051541403bb1a967672d826d9f9f5
Aaron Raddon <aaron@lytics.io>
2018-05-06 15:38:02 -0700

    sqlite wip

--- datasource/sqlite/conn.go
+++ datasource/sqlite/conn.go
@@ -8,23 +8,24 @@ import (
 	"sync"
 
 	u "github.com/araddon/gou"
+	"github.com/dchest/siphash"
 	"github.com/google/btree"
 	_ "github.com/mattn/go-sqlite3"
 	"golang.org/x/net/context"
 
 	"github.com/araddon/qlbridge/datasource"
+	"github.com/araddon/qlbridge/exec"
 	"github.com/araddon/qlbridge/expr"
 	"github.com/araddon/qlbridge/plan"
 	"github.com/araddon/qlbridge/rel"
 	"github.com/araddon/qlbridge/schema"
-	"github.com/araddon/qlbridge/value"
-	"github.com/araddon/qlbridge/vm"
 )
 
 var (
 	// ensure our conn implements connection features
-	_ schema.Conn    = (*conn)(nil)
-	_ schema.ConnAll = (*tblconn)(nil)
+	_ schema.Conn         = (*conn)(nil)
+	_ schema.ConnAll      = (*tblconn)(nil)
+	_ schema.ConnMutation = (*tblconn)(nil)
 )
 
 // conn implements qlbridge schema.Conn to a sqlite file based source.
@@ -37,12 +38,20 @@ type conn struct {
 	tblconns map[string]*tblconn
 }
 
-// tblconn
+// tblconn is a single-table connection in order to manage
+// stateful, non-multi-threaded access to tables.
 type tblconn struct {
-	exit <-chan bool
-	conn *conn
-	tbl  *schema.Table
-	rows *sql.Rows
+	*exec.TaskBase
+	stmt     rel.SqlStatement
+	exit     <-chan bool
+	conn     *conn
+	tbl      *schema.Table
+	indexCol int
+	rows     *sql.Rows
+	ct       uint64
+	cols     []string
+	colidx   map[string]int
+	err      error
 }
 
 func newConn(s *schema.Schema) *conn {
@@ -117,22 +126,44 @@ func (m *tblconn) Length() int                     { return 0 }
 
 //func (m *conn) SetColumns(cols []string)                  { m.tbl.SetColumns(cols) }
 
+// CreateMutator part of Mutator interface to allow this connection to have access
+// to the full plan context to take original sql statement and pass through to sqlite.
+func (m *tblconn) CreateMutator(pc interface{}) (schema.ConnMutator, error) {
+	if ctx, ok := pc.(*plan.Context); ok && ctx != nil {
+		m.TaskBase = exec.NewTaskBase(ctx)
+		m.stmt = ctx.Stmt
+		return m, nil
+	}
+	return nil, fmt.Errorf("Expected *plan.Context but got %T", pc)
+}
+
 func (m *tblconn) Next() schema.Message {
 	//u.Infof("Next()")
+	if m.rows == nil {
+		m.err = fmt.Errorf("wtf missing rows")
+		return nil
+	}
 	select {
 	case <-m.exit:
 		return nil
 	default:
 		for {
-
-			if item == nil {
-				//u.Debugf("reset cursor to nil  %#v", item)
+			if !m.rows.Next() {
+				return nil
+			}
+			vals := make([]driver.Value, len(m.cols))
+			m.err = m.rows.Scan(&vals)
+			if m.err != nil {
 				return nil
 			}
 
+			msg := datasource.NewSqlDriverMessageMap(m.ct, vals, m.colidx)
+
+			m.ct++
+
 			//u.Infof("return item btreeP:%p itemP:%p cursorP:%p  %v %v", m, item, m.cursor, msg.Id(), msg.Values())
 			//u.Debugf("return? %T  %v", item, item.(*DriverItem).SqlDriverMessageMap)
-			return msg.SqlDriverMessageMap.Copy()
+			return msg
 		}
 	}
 }
@@ -140,7 +171,7 @@ func (m *tblconn) Next() schema.Message {
 // interface for Upsert.Put()
 func (m *tblconn) Put(ctx context.Context, key schema.Key, row interface{}) (schema.Key, error) {
 
-	//u.Infof("%p Put(),  row:%#v", m, row)
+	u.Infof("%p Put(),  row:%#v", m, row)
 	switch rowVals := row.(type) {
 	case []driver.Value:
 		if len(rowVals) != len(m.Columns()) {
@@ -148,62 +179,9 @@ func (m *tblconn) Put(ctx context.Context, key schema.Key, row interface{}) (sch
 			return nil, fmt.Errorf("Wrong number of columns, got %v expected %v", len(rowVals), len(m.Columns()))
 		}
 		id := makeId(rowVals[m.indexCol])
-		sdm := datasource.NewSqlDriverMessageMap(id, rowVals, m.tbl.FieldPositions)
-		item := DriverItem{sdm}
-		itemResult := m.bt.ReplaceOrInsert(&item)
-		if itemResult != nil {
-			//u.Errorf("could not insert? %#v", itemResult)
-		}
+		//sdm := datasource.NewSqlDriverMessageMap(id, rowVals, m.tbl.FieldPositions)
+		//m.conn.db.Exec(m.stmt.String(), nil)
 		//u.Debugf("%p  PUT: id:%v IdVal:%v  Id():%v vals:%#v", m, id, sdm.IdVal, sdm.Id(), rowVals)
-		return NewKey(id), nil
-	case map[string]driver.Value:
-		// We need to convert the key:value to []driver.Value so
-		// we need to look up column index for each key, and write to vals
-
-		// TODO:   if this is a partial update, we need to look up vals
-		row := make([]driver.Value, len(m.Columns()))
-		if len(rowVals) < len(m.Columns()) {
-			// How do we get the key?
-			//m.Get(key)
-		}
-
-		for key, val := range rowVals {
-			if keyIdx, ok := m.tbl.FieldPositions[key]; ok {
-				row[keyIdx] = val
-			} else {
-				return nil, fmt.Errorf("Found column in Put that doesn't exist in cols: %v", key)
-			}
-		}
-		id := uint64(0)
-		if key == nil {
-			if row[m.indexCol] == nil {
-				// Since we do not have an indexed column to work off of,
-				// the ideal would be to get the job builder/planner to do
-				// a scan with whatever info we have and feed that in?   Instead
-				// of us implementing our own scan?
-				u.Warnf("wtf, nil key? %v %v", m.indexCol, row)
-				return nil, fmt.Errorf("cannot update on non index column ")
-			}
-			id = makeId(row[m.indexCol])
-		} else {
-			id = makeId(key)
-			sdm, _ := m.Get(key)
-			//u.Debugf("sdm: %#v  err%v", sdm, err)
-			if sdm != nil {
-				if dmval, ok := sdm.Body().(*datasource.SqlDriverMessageMap); ok {
-					for i, val := range dmval.Values() {
-						if row[i] == nil {
-							row[i] = val
-						}
-					}
-				}
-			}
-		}
-		//u.Debugf("PUT: %#v", row)
-		//u.Infof("PUT: %v  key:%v  row:%v", id, key, row)
-		sdm := datasource.NewSqlDriverMessageMap(id, row, m.tbl.FieldPositions)
-		item := DriverItem{sdm}
-		m.bt.ReplaceOrInsert(&item)
 		return NewKey(id), nil
 	default:
 		u.Warnf("not implemented %T", row)
@@ -221,84 +199,116 @@ func (m *tblconn) CanSeek(sql *rel.SqlSelect) bool {
 }
 
 func (m *tblconn) Get(key driver.Value) (schema.Message, error) {
-	item := m.bt.Get(NewKey(makeId(key)))
-	if item != nil {
-		return item.(*DriverItem).SqlDriverMessageMap, nil
+
+	row := m.conn.db.QueryRow(fmt.Sprintf("SELECT * FROM %v WHERE %s = $1", "hello", "damn"), key)
+	vals := make([]driver.Value, len(m.cols))
+	if err := row.Scan(&vals); err != nil {
+		return nil, err
 	}
-	return nil, schema.ErrNotFound // Should not found be an error?
+	return datasource.NewSqlDriverMessageMap(0, vals, m.colidx), nil
 }
 
 func (m *tblconn) MultiGet(keys []driver.Value) ([]schema.Message, error) {
-	rows := make([]schema.Message, len(keys))
-	for i, key := range keys {
-		item := m.bt.Get(NewKey(makeId(key)))
-		if item == nil {
-			return nil, schema.ErrNotFound
-		}
-		rows[i] = item.(*DriverItem).SqlDriverMessageMap
-	}
-	return rows, nil
+	return nil, schema.ErrNotImplemented
 }
 
 // Interface for Deletion
 func (m *tblconn) Delete(key driver.Value) (int, error) {
-	item := m.bt.Delete(NewKey(makeId(key)))
-	if item == nil {
-		//u.Warnf("could not delete: %v", key)
-		return 0, schema.ErrNotFound
-	}
-	return 1, nil
+	// item := m.bt.Delete(NewKey(makeId(key)))
+	// if item == nil {
+	// 	//u.Warnf("could not delete: %v", key)
+	// 	return 0, schema.ErrNotFound
+	// }
+	// return 1, nil
+	return -1, schema.ErrNotImplemented
 }
 
 // DeleteExpression Delete using a Where Expression
 func (m *tblconn) DeleteExpression(p interface{}, where expr.Node) (int, error) {
 
-	_, ok := p.(*plan.Delete)
-	if !ok {
-		return 0, plan.ErrNoPlan
-	}
-
-	deletedKeys := make([]*Key, 0)
-	m.bt.Ascend(func(a btree.Item) bool {
-		di, ok := a.(*DriverItem)
+	return -1, schema.ErrNotImplemented
+	/*
+		_, ok := p.(*plan.Delete)
 		if !ok {
-			u.Warnf("wat?  %T   %#v", a, a)
-			return false
+			return 0, plan.ErrNoPlan
 		}
-		msgCtx := di.SqlDriverMessageMap
-		whereValue, ok := vm.Eval(msgCtx, where)
-		if !ok {
-			u.Debugf("could not evaluate where: %v", msgCtx.Values())
-			//return deletedCt, fmt.Errorf("Could not evaluate where clause")
+
+		deletedKeys := make([]*Key, 0)
+		m.bt.Ascend(func(a btree.Item) bool {
+			di, ok := a.(*DriverItem)
+			if !ok {
+				u.Warnf("wat?  %T   %#v", a, a)
+				return false
+			}
+			msgCtx := di.SqlDriverMessageMap
+			whereValue, ok := vm.Eval(msgCtx, where)
+			if !ok {
+				u.Debugf("could not evaluate where: %v", msgCtx.Values())
+				//return deletedCt, fmt.Errorf("Could not evaluate where clause")
+				return true
+			}
+			switch whereVal := whereValue.(type) {
+			case value.BoolValue:
+				if whereVal.Val() == false {
+					//this means do NOT delete
+				} else {
+					// Delete!
+					indexVal := msgCtx.Values()[m.indexCol]
+					deletedKeys = append(deletedKeys, NewKey(makeId(indexVal)))
+				}
+			case nil:
+				// ??
+			default:
+				if whereVal.Nil() {
+					// Doesn't match, so don't delete
+				} else {
+					u.Warnf("unknown type? %T", whereVal)
+				}
+			}
 			return true
-		}
-		switch whereVal := whereValue.(type) {
-		case value.BoolValue:
-			if whereVal.Val() == false {
-				//this means do NOT delete
-			} else {
-				// Delete!
-				indexVal := msgCtx.Values()[m.indexCol]
-				deletedKeys = append(deletedKeys, NewKey(makeId(indexVal)))
-			}
-		case nil:
-			// ??
-		default:
-			if whereVal.Nil() {
-				// Doesn't match, so don't delete
-			} else {
-				u.Warnf("unknown type? %T", whereVal)
-			}
-		}
-		return true
-	})
+		})
 
-	for _, deleteKey := range deletedKeys {
-		if ct, err := m.Delete(deleteKey); err != nil {
-			u.Errorf("Could not delete key: %v", deleteKey)
-		} else if ct != 1 {
-			u.Errorf("delete should have removed 1 key %v", deleteKey)
+		for _, deleteKey := range deletedKeys {
+			if ct, err := m.Delete(deleteKey); err != nil {
+				u.Errorf("Could not delete key: %v", deleteKey)
+			} else if ct != 1 {
+				u.Errorf("delete should have removed 1 key %v", deleteKey)
+			}
 		}
+		return len(deletedKeys), nil
+	*/
+}
+
+func makeId(dv driver.Value) uint64 {
+	switch vt := dv.(type) {
+	case int:
+		return uint64(vt)
+	case int64:
+		return uint64(vt)
+	case []byte:
+		return siphash.Hash(456729, 1111581582, vt)
+	case string:
+		return siphash.Hash(456729, 1111581582, []byte(vt))
+		//by := append(make([]byte,0,8), byte(r), byte(r>>8), byte(r>>16), byte(r>>24), byte(r>>32), byte(r>>40), byte(r>>48), byte(r>>56))
+	case datasource.KeyCol:
+		return makeId(vt.Val)
 	}
-	return len(deletedKeys), nil
+	return 0
+}
+
+// Key implements Key and Sort interfaces.
+type Key struct {
+	Id uint64
+}
+
+func NewKey(key uint64) *Key     { return &Key{key} }
+func (m *Key) Key() driver.Value { return driver.Value(m.Id) }
+func (m *Key) Less(than btree.Item) bool {
+	switch it := than.(type) {
+	case *Key:
+		return m.Id < it.Id
+	default:
+		u.Warnf("what type? %T", than)
+	}
+	return false
 }
--- datasource/sqlite/sqlite_test.go
+++ datasource/sqlite/sqlite_test.go
@@ -3,6 +3,7 @@ package sqlite_test
 import (
 	"testing"
 
+	u "github.com/araddon/gou"
 	td "github.com/araddon/qlbridge/datasource/mockcsvtestdata"
 	_ "github.com/mattn/go-sqlite3"
 
@@ -20,6 +21,22 @@ func init() {
 	testutil.Setup()
 	// load our mock data sources "users", "articles"
 	td.LoadTestDataOnce()
+
+	for _, tablename := range td.MockSchema.Tables() {
+		tbl, _ := td.MockSchema.Table(tablename)
+		if tbl == nil {
+			panic("missing table " + tablename)
+		}
+		u.Infof("found schema for %s \n%s", tablename, tbl.String())
+	}
+
+	td.TestContext = func(query string) *plan.Context {
+		ctx := plan.NewContext(query)
+		ctx.DisableRecover = true
+		//ctx.Schema = MockSchema
+		ctx.Session = datasource.NewMySqlSessionVars()
+		return ctx
+	}
 }
 
 func planContext(query string) *plan.Context {
