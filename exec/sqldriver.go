e3c940a0103fed7732ba29eefc45ff519e6f2113
Guy Molinari <guy.molinari@disney.com>
2020-10-20 14:05:39 +0000

    Use original SQL projection list for driver results output.

--- exec/sqldriver.go
+++ exec/sqldriver.go
@@ -248,7 +248,7 @@ func (m *qlbStmt) Query(args []driver.Value) (driver.Rows, error) {
 	// The only type of stmt that makes sense for Query is SELECT
 	//  and we need list of columns that requires casing
 	//sqlSelect, ok := job.Ctx.Stmt.(*rel.SqlSelect)
-	_, ok := job.Ctx.Stmt.(*rel.SqlSelect)
+	sqlSelect, ok := job.Ctx.Stmt.(*rel.SqlSelect)
 	if !ok {
 		u.Warnf("ctx? %v", job.Ctx)
 		return nil, fmt.Errorf("We could not recognize that as a select query: %T", job.Ctx.Stmt)
@@ -256,14 +256,16 @@ func (m *qlbStmt) Query(args []driver.Value) (driver.Rows, error) {
 
 	// Prepare a result writer, we manually append this task to end
 	// of job?
-	//resultWriter := NewResultRows(ctx, sqlSelect.Columns.AliasedFieldNames())
+	resultWriter := NewResultRows(ctx, sqlSelect.Columns.AliasedFieldNames())
 
+/*
     projCols := job.Ctx.Projection.Proj.Columns
 	cols := make([]string, len(projCols))
 	for i, col := range projCols {
 		cols[i] = col.As
 	}
 	resultWriter := NewResultRows(ctx, cols)
+*/
 
 	job.RootTask.Add(resultWriter)
 
